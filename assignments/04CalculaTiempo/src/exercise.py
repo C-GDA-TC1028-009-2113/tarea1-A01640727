08bef05a76e6c70c108d001a12df5482e8ee41db
A01640727 <A01640727@itesm.mx>
2021-08-20 23:59:25 +0000

    Tarea 1

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    edad = int(input("Dame tu edad: "))
+    year = int(input("Dame el año actual: "))
+    year_100 = year+(100-edad)
+    print("Cumplirás 100 años en el año:",year_100)
 
 
 
--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    peso_in = float(input("Dame el peso inicial: "))
+    peso_fin = float(input("Dame el peso final: "))
+    meses = int(input("Dame la cantidad de meses: "))
+    por_mes = (peso_in - peso_fin)/meses
+    print("Lo que debes bajar por mes es:",por_mes)
+
 
 
 
